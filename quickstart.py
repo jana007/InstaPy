5df1b0b8ff1719b770ce5becf5851f9720265de4
Jana McKinnon <jmckinnon@campus.fsu.edu>
2019-11-15 10:42:12 -0500

    Update quickstart.py
    thumbs up

--- quickstart.py
+++ quickstart.py
@@ -14,7 +14,22 @@ session = InstaPy()
 
 with smart_run(session):
     # general settings
-    session.set_dont_include(["friend1", "friend2", "friend3"])
+    # session.set_dont_include(["friend1", "friend2", "friend3"])
 
     # activity
-    session.like_by_tags(["natgeo"], amount=10)
+    session.like_by_tags(["thumbsup"], amount=15)
+    session.set_do_comment(enabled=True, percentage=75)
+    session.set_comments(['thumbs up', u'thumbs up 👍', u'👍thumbs up👍', u'👍👍👍👍👍👍👍'])
+    
+    
+    session.set_quota_supervisor(enabled=True, sleep_after=["likes", "comments_d", "follows", "unfollows", "server_calls_h"], sleepyhead=True, stochastic_flow=True, notify_me=True,
+                              peak_likes_hourly=57,
+                              peak_likes_daily=585,
+                               peak_comments_hourly=21,
+                               peak_comments_daily=182,
+                                peak_follows_hourly=48,
+                                peak_follows_daily=None,
+                                 peak_unfollows_hourly=35,
+                                 peak_unfollows_daily=402,
+                                  peak_server_calls_hourly=None,
+                                  peak_server_calls_daily=4700)
